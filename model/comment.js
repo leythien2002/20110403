a7454331686476e774f70fc61820666b61b76c50
ThienStu <leythien2002stu@gmail.com>
2023-10-03 21:55:29 +0700

    SimpleBlog

---
+++ model/comment.js
@@ -0,0 +1,22 @@
+const commentList = [
+  {
+    id: 1,
+    postId: 1,
+    author: "Lê Y Thiện",
+    content: "Comment 1",
+  },
+
+  {
+    id: 2,
+    postId: 1,
+    author: "Thiện Lê",
+    content: "Comment 2",
+  },
+];
+
+let commentSequence = 3;
+
+module.exports = {
+  commentList,
+  commentSequence,
+};
---
+++ model/post.js
@@ -0,0 +1,14 @@
+const postList = [
+  {
+    id: Number(1),
+    title: "First Post",
+    author: "Lê Y Thiện",
+    content: "First post",
+  },
+];
+let postSequence = postList.length + 1;
+
+module.exports = {
+  postList,
+  postSequence,
+};
