949a8eb6092e78e8a83d40e34936939e5e834c0e
ThienStu <leythien2002stu@gmail.com>
2023-10-29 13:09:42 +0700

    Init

---
+++ server/src/server.js
@@ -0,0 +1,16 @@
+const PORT= process.env.PORT||8000;
+
+const http=require('http');
+const app=require('./app');;
+
+const {loadPlanetsData}=require('./models/planets.model')
+
+const server=http.createServer(app);
+async function startServer(){
+    await loadPlanetsData();
+    server.listen(PORT,()=>{
+        console.log(`Listen on port : ${PORT}`);
+    });
+}
+startServer();
+
